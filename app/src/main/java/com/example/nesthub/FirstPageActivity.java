70ef5b99d1e225210644176d2038512579e5425b
pootinx <abdellahamraoui07@gmail.com>
2023-10-25 15:22:20 +0100

    Edite bottom navbar and fragment

--- app/src/main/java/com/example/nesthub/FirstPageActivity.java
+++ app/src/main/java/com/example/nesthub/FirstPageActivity.java
@@ -1,6 +1,51 @@
 package com.example.nesthub;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class FirstPageActivity extends Activity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class FirstPageActivity extends AppCompatActivity {
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    com.google.android.material.button.MaterialButton button;
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null){
+            Intent intent = new Intent(FirstPageActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firstpage);
+        button = findViewById(R.id.btn_begin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    public void logout(View view){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(FirstPageActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+
 }
